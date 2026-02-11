dd146862d26dcd7d0b609b094ce0c96b7d0dc971
yaqiang.sun <sunyaking@163.com>
2026-02-11 12:08:58 +0800

    add plot to resources quark

---
+++ resources/quark/anaylsis/visualization.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2026 yaqiang.sun.
+# This source code is licensed under the license found in the LICENSE file
+# in the root directory of this source tree.
+#########################################################################
+# Author: yaqiangsun
+# Created Time: 2026/02/11 12:00:26
+########################################################################
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from qubitclient.draw.pltmanager import QuantumPlotPltManager
+from qubitclient.draw.plymanager import QuantumPlotPlyManager
+from qubitclient import TaskName,NNTaskName
+
+from datetime import date
+from pathlib import Path
+import os
+import logging
+from .config import API_URL,API_KEY,ENABLE_API
+from .wrapper_handler import handle_exceptions, control_api_execution
+
+from .format import optpipulse_convert
+
+def plot_template(data,results,save_path,task_type=TaskName.S21PEAK):
+
+    if type(results)==dict:
+        results = results.get("results")
+    image = data
+    dict_list = [np.array(image)]
+
+    ply_plot_manager = QuantumPlotPlyManager()
+    plt_plot_manager = QuantumPlotPltManager()
+    print("plotting ai image...")
+    print(save_path)
+
+    for idx, (result, dict_param) in enumerate(zip(results, dict_list)):
+        save_path_png = str(save_path)
+        save_path_html = save_path_png.replace("png",".html")
+        fig_plt = plt_plot_manager.plot_quantum_data(
+            data_type='npy',
+            task_type=task_type.value,
+            save_path=save_path_png,
+            result=result,
+            dict_param=dict_param
+        )
+        fig_ply = ply_plot_manager.plot_quantum_data(
+            data_type='npy',
+            task_type=task_type.value,
+            save_path=save_path_html,
+            result=result,
+            dict_param=dict_param
+        )
+    logging.info(f"Saving ai image to:{save_path}")
+
+def plot_optpipulse(data,results,save_path):
+    data = optpipulse_convert(data)
+    plot_template(data,results,save_path,task_type=TaskName.OPTPIPULSE)
--- tests/resources/quark/test_analysis.py
+++ tests/resources/quark/test_analysis.py
@@ -16,22 +16,24 @@ if project_root not in sys.path:
     sys.path.insert(0, project_root)
 from resources.quark.anaylsis.utils import get_pkl_content
 from resources.quark.anaylsis.inception import optpipulse
+from resources.quark.anaylsis.visualization import plot_optpipulse
 
 def main(task_key="pi"):
     base_dir = "tmp/data"
     for pkl_path in os.listdir(base_dir):
         pkl_path = os.path.join(base_dir, pkl_path)
-        result = get_pkl_content(pkl_path)
-        if result is None:
+        data = get_pkl_content(pkl_path)
+        if data is None:
             continue
-        if "meta" not in result.keys():
+        if "meta" not in data.keys():
             continue
-        if "name" not in result["meta"].keys():
+        if "name" not in data["meta"].keys():
             continue
-        if task_key.lower() in result["meta"]["name"].lower():
-            if len(result["meta"]["other"]["qubits"])>1:
+        if task_key.lower() in data["meta"]["name"].lower():
+            if len(data["meta"]["other"]["qubits"])>1:
                 if task_key in "opt_pipulse":
-                    optpipulse(result)
+                    analysis_result = optpipulse(data)
+                    plot_optpipulse(data,analysis_result,save_path='./tmp/vis/opt_pipulse.png')
 
 if __name__ == "__main__":
     main()

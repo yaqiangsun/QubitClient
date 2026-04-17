131fb2d20e8092b310c07bc860810d6bb0a625ae
yaqiang.sun <sunyaking@163.com>
2026-04-17 12:23:09 +0800

    add: code generation module

---
+++ qubitclient/generate/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2026 yaqiang.sun.
+# This source code is licensed under the license found in the LICENSE file
+# in the root directory of this source tree.
+#########################################################################
+# Author: yaqiangsun
+# Created Time: 2026/04/17 12:22:49
+########################################################################
